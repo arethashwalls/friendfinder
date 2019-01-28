7d05e06e2cc55d8230a74bb8efa63f8e7e7ac308
ASH Walls <admin@arethawalls.com>
2019-01-28 15:52:18 -0700

    More work on popup and api call

--- app/public/assets/script.js
+++ app/public/assets/script.js
@@ -14,10 +14,29 @@ document.querySelector("#survey").addEventListener("submit", (e) => {
         yourProfile.scores.push(parseInt(document.querySelector(`input[name="q${i}"]:checked`).value))
     }
 
+    //Display your profile on the survey page pop-up:
+    document.querySelector('#profile-popup').style.display = 'block';
+    document.querySelector('#your-name').textContent = yourProfile.name;
+    document.querySelector('#your-pic').src = yourProfile.photo;
+
     //Make a POST request with the yourProfile object:
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", '/api/friends', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(yourProfile));
+    fetch('/api/friends', {
+        method: 'POST',
+        body: JSON.stringify(yourProfile),
+        headers:{ 'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+    //The POST request should return your match. Display the match in the pop-up:
+    .then(data => {
+        console.log(data);
+        document.querySelector('#match-name').textContent = data.name;
+        document.querySelector('#match-pic').src = data.photo;
+    });
+    
 });
 //**************************************************************************************//  
+
+//Add a listener allowing users to close the profile popup:
+document.querySelector('#close-profile').addEventListener('click', (e) => {
+    document.querySelector('#profile-popup').style.display = 'none';
+});
